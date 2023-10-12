b1c39d0ef1b02ea57756118e374650eca1e25727
Julie9997 <ula.9997@mail.ru>
2023-10-12 18:48:35 +0300

    tests 2

---
+++ Test/seminars/second/hw/VehicleTest.java
@@ -0,0 +1,61 @@
+package seminars.second.hw;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+
+    private Car car;
+    private Motorcycle motorcycle;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("Dodge", "Ram", 2010);
+        motorcycle = new Motorcycle("Yamaha", "R1", 2015);
+    }
+
+    @Test
+    public void testCarIsInstanceOfVehicle() {
+        assertTrue(car instanceof Vehicle);
+    }
+
+    @Test
+    void testCarNumWheels() {
+        assertThat(car.getNumWheels()).isEqualTo(4);
+    }
+
+    @Test
+    void testMotoNumWheels() {
+        assertThat(motorcycle.getNumWheels()).isEqualTo(2);
+    }
+
+    @Test
+    void testCarSpeedTestDrive() {
+        car.testDrive();
+        assertThat(car.getSpeed()).isEqualTo(60);
+    }
+
+    @Test
+    void testMotoSpeedTestDrive() {
+        motorcycle.testDrive();
+        assertThat(motorcycle.getSpeed()).isEqualTo(75);
+    }
+
+    @Test
+    void testCarSpeedParking() {
+        car.testDrive();
+        car.park();
+        assertThat(car.getSpeed()).isEqualTo(0);
+    }
+
+    @Test
+    void testMotoSpeedParking() {
+        motorcycle.testDrive();
+        motorcycle.park();
+        assertThat(motorcycle.getSpeed()).isEqualTo(0);
+    }
+
+}
